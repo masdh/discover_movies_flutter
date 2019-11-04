8480fd5d49a0ded155211d2523468b389c259ebb
Sweatysven <devanhansendk@gmail.com>
2019-11-04 15:59:52 +0100

    files added

---
+++ movieapp/lib/ui/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../util/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: pureWhite,
+       body: Stack(
+         children: <Widget>[
+
+         ],
+       ),
+    );
+  }
+}
